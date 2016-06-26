1c8ca16719c23a354b45c416b7f05e5d654f1969
Waters <h2oboi89@gmail.com>
2016-06-26 15:06:08 -0400

    Updated documentation and version bump

--- src/loops.js
+++ src/loops.js
@@ -10,6 +10,7 @@
  * @param {function} update Executes after the loop body. ie: `() => i++`
  * @param {function} thunk Loop body. Returns a Promise and accepts a value.
  * @param {object} [seed] Initial value to be passed to first loop iteration.
+ * @returns {object} Resolves with final resolved value.
  */
 let forLoop = (initial, condition, update, thunk, seed) => {
   initial();
@@ -24,6 +25,7 @@ let forLoop = (initial, condition, update, thunk, seed) => {
  * @param {function} condition Checks whether loop body should be executed. ie: `() => i < 10`
  * @param {function} thunk Loop body. Returns a Promise and accepts a value.
  * @param {object} [seed] Initial value to be passed to first loop iteration.
+ * @returns {object} Resolves with final resolved value.
  */
 let whileLoop = (condition, thunk, seed) => {
   return _loop(condition, () => {}, thunk, seed);
@@ -55,6 +57,7 @@ let _loop = (condition, update, thunk, value) => {
  * @param {function} condition Checks whether loop should continue or quit. ie: `() => i < 10`
  * @param {function} thunk Loop body. Returns a Promise and accepts a value.
  * @param {object} [seed] Initial value to be passed to first loop iteration.
+ * @returns {object} Resolves with final resolved value.
  */
 let doWhileLoop = (condition, thunk, seed) => {
   return _doLoop(condition, thunk, seed);
@@ -79,13 +82,19 @@ let _doLoop = (condition, thunk, value) => {
 
 /**
  * Asynchronous `forEach` loop.
- * The initial iteration is passed the `seed` value and an item from `items`.
- * Subsequent iterations get passed the resolved value from the last iteration
- * and the next item from `items`.
+ * Each iteration is passed a the following: `(value, items[index], index, items)`.
+ * For the first iteration the value is the `seed` value.
+ * Subsequent iterations get the resolved value from the previous iteration.
+ * Index starts at 0 and is incremented each iteration.
+ * After each iteration the current item in `items` is updated with the resolved
+ * value of the current iteration.
+ * NOTE: a copy of the input `items` array is made at the beginning and the original
+ * array will remain unchanged as all work is done on the copy.
  * @function forEach
  * @param {object[]} items Collection to iterator over.
  * @param {function} thunk Loop body. Returns a Promise and accepts a value.
  * @param {object} [seed] Initial value to be passed to first loop iteration.
+ * @returns {object[]} Resolves with updated `items` array.
  */
 let forEachLoop = (items, thunk, seed) => {
   return _forEachLoop(items.slice(), thunk, 0, seed);
