43f03dbbc05d8c9c125ba550c65f0acb3c97f002
Waters <h2oboi89@gmail.com>
2016-07-08 19:15:59 -0400

    Added more explanatory comments to control.js

--- sample/control.js
+++ sample/control.js
@@ -9,13 +9,13 @@ let i = 0;
 
 // loop body
 let body = (value) => {
-  value = value + (i++);
+  value += (i++);
 
   if(value % 2 === 0) {
     // promise will stop execution and resolve value of the last loop iteration.
     return Promise.reject(loops.continue);
   }
-  else if(value > 100) {
+  else if(value > 10) {
     // promise will resolve with value of last loop iteration
     // in essence, this iteration does not count
     return Promise.reject(loops.break);
@@ -31,5 +31,12 @@ let seed = 1;
 
 loops.while(condition, body, seed)
   .then((sum) => {
+    // 1 + 3 + 3 => 7
+    //
+    // all iterations input: [1, 1, 1, 3, 3, 7, 7]
+    // all iterations i:     [0, 1, 2, 3, 4, 5, 6]
+    // value after line 12:  [1, 2, 3, 6, 7, 12, 13]
+    // 2, 6, & 12 are rejected (mach.continue) because they are divisible by 2 (value % 2 === 0)
+    // 13 is rejected (mach.break) because it is greater than 10
     console.log(sum);
   });
